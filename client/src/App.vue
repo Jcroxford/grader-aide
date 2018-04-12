394f9734c11b23f4a8e0eb3f51e8ce3327e7875e
Jcroxford <takeflightsc@gmail.com>
2018-04-12 17:58:34 -0600

    add page animation

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,9 @@
   <v-app>
     <nav-bar></nav-bar>
     <div id="app">
-      <router-view/>
+      <transition name="slide" mode="out-in">
+        <router-view/>
+      </transition>
     </div>
   </v-app>
 </template>
@@ -19,4 +21,30 @@ export default {
 </script>
 
 <style>
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+}
 </style>
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,119 +1,119 @@
 <template>
   <div class="login-container main-background">
-  <div class="navbar-offest">
-    <v-layout>
-      <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-      <template>
-        <div class="login-div">
-          <v-tabs
-            color="dark"
-            dark
-            slider-color="yellow"
-            centered
-          >
-          <v-tab
-            :key="'login'"
-            ripple
-          >
-            Login
-          </v-tab>
-          <v-tab-item
-            :key="'login'"
-          >
-            <v-card flat>
-            <v-card-text>
-              <v-form ref="form" lazy-validation>
-                <v-text-field
-                  label="E-mail"
-                  v-model="email"
-                  :rules="emailRules"
-                  required
-                ></v-text-field>
-                <v-text-field
-                  label="Password"
-                  v-model="password"
-                  :rules="passwordRules"
-                  :append-icon="hidePassword ? 'visibility' : 'visibility_off'"
-                  :append-icon-cb="() => (hidePassword = !hidePassword)"
-                  :type="hidePassword ? 'password' : 'text'"
-                  required
-                ></v-text-field>
+    <div class="navbar-offest">
+      <v-layout>
+        <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+        <template>
+          <div class="login-div">
+            <v-tabs
+              color="dark"
+              dark
+              slider-color="yellow"
+              centered
+            >
+            <v-tab
+              :key="'login'"
+              ripple
+            >
+              Login
+            </v-tab>
+            <v-tab-item
+              :key="'login'"
+            >
+              <v-card flat>
+              <v-card-text>
+                <v-form ref="form" lazy-validation>
+                  <v-text-field
+                    label="E-mail"
+                    v-model="email"
+                    :rules="emailRules"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    label="Password"
+                    v-model="password"
+                    :rules="passwordRules"
+                    :append-icon="hidePassword ? 'visibility' : 'visibility_off'"
+                    :append-icon-cb="() => (hidePassword = !hidePassword)"
+                    :type="hidePassword ? 'password' : 'text'"
+                    required
+                  ></v-text-field>
 
-                <v-btn
-                  @click="login"
-                  class="centered"
-                  color="success"
-                >
-                Login
-                </v-btn>
-              </v-form>
-            </v-card-text>
-            </v-card>
-          </v-tab-item>
-          <v-tab
-            :key="'register'"
-            ripple
-          >
-            Register
-          </v-tab>
-          <v-tab-item
-            :key="'register'"
-          >
-            <v-card flat>
-            <v-card-text>
-              <v-form ref="form" lazy-validation>
-                <v-text-field
-                  label="Full Name"
-                  v-model="name"
-                  :rules="nameRules"
-                  required
-                ></v-text-field>
-                <v-text-field
-                  label="E-mail"
-                  v-model="email"
-                  :rules="emailRules"
-                  required
-                ></v-text-field>
-                <v-text-field
-                  label="Password"
-                  v-model="password"
-                  :rules="passwordRules"
-                  :append-icon="hidePassword ? 'visibility' : 'visibility_off'"
-                  :append-icon-cb="() => (hidePassword = !hidePassword)"
-                  :type="hidePassword ? 'password' : 'text'"
-                  required
-                ></v-text-field>
-                <v-text-field
-                  label="Confirm Password"
-                  v-model="confirmPassword"
-                  :rules="confirmPasswordRules"
-                  :append-icon="hideConfirmPassword ? 'visibility' : 'visibility_off'"
-                  :append-icon-cb="() => (hideConfirmPassword = !hideConfirmPassword)"
-                  :type="hideConfirmPassword ? 'password' : 'text'"
-                  required
-                ></v-text-field>
+                  <v-btn
+                    @click="login"
+                    class="centered"
+                    color="success"
+                  >
+                  Login
+                  </v-btn>
+                </v-form>
+              </v-card-text>
+              </v-card>
+            </v-tab-item>
+            <v-tab
+              :key="'register'"
+              ripple
+            >
+              Register
+            </v-tab>
+            <v-tab-item
+              :key="'register'"
+            >
+              <v-card flat>
+              <v-card-text>
+                <v-form ref="form" lazy-validation>
+                  <v-text-field
+                    label="Full Name"
+                    v-model="name"
+                    :rules="nameRules"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    label="E-mail"
+                    v-model="email"
+                    :rules="emailRules"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    label="Password"
+                    v-model="password"
+                    :rules="passwordRules"
+                    :append-icon="hidePassword ? 'visibility' : 'visibility_off'"
+                    :append-icon-cb="() => (hidePassword = !hidePassword)"
+                    :type="hidePassword ? 'password' : 'text'"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    label="Confirm Password"
+                    v-model="confirmPassword"
+                    :rules="confirmPasswordRules"
+                    :append-icon="hideConfirmPassword ? 'visibility' : 'visibility_off'"
+                    :append-icon-cb="() => (hideConfirmPassword = !hideConfirmPassword)"
+                    :type="hideConfirmPassword ? 'password' : 'text'"
+                    required
+                  ></v-text-field>
 
-                <v-btn
-                  @click="register"
-                  class="centered"
-                  color="success"
-                >
-                Register
-                </v-btn>
-              </v-form>
-            </v-card-text>
-            </v-card>
-          </v-tab-item>
-          </v-tabs>
+                  <v-btn
+                    @click="register"
+                    class="centered"
+                    color="success"
+                  >
+                  Register
+                  </v-btn>
+                </v-form>
+              </v-card-text>
+              </v-card>
+            </v-tab-item>
+            </v-tabs>
 
-        </div>
-      </template>
-      </v-card>
-      </v-flex>
-    </v-layout>
-  </div>
-      <v-snackbar
+          </div>
+        </template>
+        </v-card>
+        </v-flex>
+      </v-layout>
+    </div>
+    <v-snackbar
       :timeout="5000"
       bottom
       v-model="snackbar"
