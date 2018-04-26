6b4b72662cfea2a6879a598cf24cdee68dad2b82
Jcroxford <takeflightsc@gmail.com>
2018-04-25 20:11:11 -0600

    added course model scaffold

---
+++ models/courses.model.js
@@ -0,0 +1,66 @@
+/**
+ * This is how a course looks
+ * @param {string} _id - unique
+ * @param {string} name
+ * @param {object[]} assignments see assignment object definition below
+ * @param {string[]} studentsEnrolled - ids of enrolled students
+ */
+
+/**
+ * This is the look of an assignment object
+ * @param {string} _id - unique
+ * @param {string} name
+ * @param {number} totalPts
+ * @param {object[]} rules takes the form { _id {string} is unique, pts {number}, desc {string} }
+ * @param {object[]} comments takes the form { _id {string} is unique, desc {string} }
+ */
+const db = require('../db');
+const ObjectId = require('mongodb').ObjectID;
+
+function preview() {
+  const collection = db.collection('courses');
+
+  return collection
+    .find({})
+    .project({ name: 1 })
+    .toArray();
+}
+
+function findById(_id) {
+  const collection = db.collection('courses');
+  // const id = ObjectId(_id);
+  console.log('id: ', typeof _id);
+
+  return collection.findOne({ _id });
+}
+// create a new course
+function createCourse(course) {
+  const collection = db.collection('courses');
+
+  return collection.insertOne(course).then(({ result, insertedId }) => {
+    if (!result.ok) throw new Error('unable to save course');
+
+    return insertedId;
+  });
+}
+
+// function editCourse(courseId, updates) {
+//   const collection = db.collection('courses')
+
+//   return collection.updateOne({ _id: })
+// }
+// edit a course by id
+// delete a course
+// add assigment to a course
+// update an assignment in a course
+// delete an assignment from a acourse
+// get all assignments for a course (name and id only?)
+// get a signle assingment from a course by id for both assignment and course
+// get students enrolled in a course
+// add a student
+
+module.exports = {
+  preview,
+  findById,
+  createCourse
+};
